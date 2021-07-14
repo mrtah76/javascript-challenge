99f1e6063e7419198ce451c49253c59b140b4b3d
Moe T <78877990+mrtah76@users.noreply.github.com>
2021-07-14 01:48:49 -0700

    basic code done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//code to display data into the table
+data.forEach(function(dataRow){
+    var table = d3.select("#ufo-table")
+    var table_body = d3.select("tbody")
+    var newRow = table_body.append("tr")
+    Object.values(dataRow).forEach(colValue => {
+            newRow.append("td").text(colValue);
+        });
+    });
+
+//code to click_handler
+    function submitHandler(){
+        d3.event.preventDefault();
+        var inputValue = d3.select("#datetime").property("value");
+        filteredData = data.filter(ufo1 => ufo1.Date === inputValue)
+        console.log(inputValue)
+           
+    };
+    d3.select("#filter-btn").on("click", submitHandler);    
