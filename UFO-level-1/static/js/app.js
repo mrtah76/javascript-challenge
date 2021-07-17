73ceeaba5bbe0876b39efdd8f155530d665f1b19
Moe T <78877990+mrtah76@users.noreply.github.com>
2021-07-16 22:22:52 -0700

    UFO-level-1 Done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,30 +5,35 @@ var tableData = data;
 // YOUR CODE HERE!
 
 
-    //updated code
-    // clearBox("table-area");
-
-    function submitHandler(){
-        // var table = d3.select("#ufo-table");
-//or use :  var table = document.all.tableid;
-
+    
+    // One Function Does it All!!
+    // First Clear the existing table then Run the Filter
+    function myFunction(){
+        clearTable();
+        submitHandler();
        
-        // clearBox("table-area");
-        d3.event.preventDefault();
-        var table = d3.select("#ufo-table")
+    }
+
+
+    // Clear the existing table
+    function clearTable(){
+        var table_body =d3.selectAll("td")
+        table_body.remove()
         
+    }
+
+    // Filter by the click
+    function submitHandler(){
         var inputValue = d3.select("#datetime").property("value");
         filteredDate = data.filter(ufo1 => ufo1.datetime === inputValue)
 
         filteredDate.forEach(function(dataRow){
-    var table = d3.select("#ufo-table")
-    
-      var table_body = d3.select("tbody")
-    var newRow = table_body.append("tr")
-    Object.values(dataRow).forEach(colValue => {
+            var table_body = d3.select("tbody")
+            var newRow = table_body.append("tr")
+            Object.values(dataRow).forEach(colValue => {
             newRow.append("td").text(colValue);
         });
     });
 };
-    d3.select("#filter-btn").on("click", submitHandler);
+    d3.select("#filter-btn").on("click", myFunction);
     
