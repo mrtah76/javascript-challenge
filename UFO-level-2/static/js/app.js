9b7b663ae1f75bd98f87af614e5ef8109c7f68e3
Moe T <78877990+mrtah76@users.noreply.github.com>
2021-07-17 23:40:39 -0700

    ufo2 filter selection done

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -8,6 +8,98 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+// Here we find unique Dates:
+const unqdate = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+  
+  var listdate = data.map(date => date.datetime);
+  const uniqueDate = listdate.filter(unqdate)
+  
+//Assign Unique Dates to Drop Down Menu to be selected
+var selectDate = document.getElementById("datetime"); 
+
+for(var i = 0; i < uniqueDate.length; i++) {
+    var opt = uniqueDate[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectDate.appendChild(el);
+};
+// Here we find unique Cities:
+const unqCiti = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+  
+  var listCiti = data.map(citi => citi.city);
+  const uniqueCiti = listCiti.filter(unqCiti)
+  
+  //Assign Unique Cities to Drop Down Menu to be selected
+var selectCiti = document.getElementById("selectcity"); 
+
+for(var i = 0; i < uniqueCiti.length; i++) {
+    var opt = uniqueCiti[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectCiti.appendChild(el);
+};
+// Here we find unique States:
+const unqState = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+  
+  var listState = data.map(state => state.state);
+  const uniqueState = listState.filter(unqState)
+  
+  //Assign Unique States to Drop Down Menu to be selected
+var selectState = document.getElementById("selectstate"); 
+
+for(var i = 0; i < uniqueState.length; i++) {
+    var opt = uniqueState[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectState.appendChild(el);
+};
+// Here we find unique Country:
+const unqcountry = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+  
+  var listCountry = data.map(country => country.country);
+  const uniqueCountry = listCountry.filter(unqcountry)
+  
+  //Assign Unique Country to Drop Down Menu to be selected
+var selectCountry = document.getElementById("selectcountry"); 
+
+for(var i = 0; i < uniqueCountry.length; i++) {
+    var opt = uniqueCountry[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectCountry.appendChild(el);
+};
+
+// Here we find unique Shapes:
+const unqshape = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+  
+  var listShape = data.map(shape => shape.shape);
+  const uniqueShape = listShape.filter(unqshape)
+  
+  //Assign Unique Shapes to Drop Down Menu to be selected
+var selectShape = document.getElementById("selectshape"); 
+
+for(var i = 0; i < uniqueShape.length; i++) {
+    var opt = uniqueShape[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectShape.appendChild(el);
+}; 
+
 
     
     // One Function Does it All!!
@@ -66,3 +158,4 @@ var tableData = data;
 
     d3.select("#filter-btn").on("click", myFunction);
 }
+
